e4edf80c130b6261e76f46b1963474dea07e2963
Arv2040 <aravcr4085@gmail.com>
2025-03-30 03:30:22 +0530

    made changes

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import dynamic from "next/dynamic";
 
-const Game = dynamic(() => import("./Game/phaser"), { ssr: false });
+const Game = dynamic(() => import("./Game/phaser"),{ssr:false,loading:()=>{ return <p>Loading</p>}});
 
 export default function Home() {
   return (
