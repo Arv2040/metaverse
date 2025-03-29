fd8cc1c662c4577d96b2c7c2fe56fc9186b6616b
GurnoorArora <gurnoora51@gmail.com>
2025-03-30 03:26:14 +0530

    added router

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,13 @@
 import  express,{Request,Response}  from "express";
 import dotenv from "dotenv"
 import mongoose from "mongoose";
+import {router} from "./routes/v1/routes"
 
 const app=express();
 dotenv.config();
 
 const PORT=process.env.PORT;
-
-app.get('/',(req: Request,res:Response)=>{
-    res.send("Home Page");
-})
-
+app.use("/api/v1",router);
 app.listen(PORT,()=>{
     console.log(`Server is running on the Port: ${PORT}`);
 })
